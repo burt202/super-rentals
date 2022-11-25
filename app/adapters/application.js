c946d6061b264e0471b7dd13140cfa4d1924e88c
Aaron Burtnyk <aaron.burtnyk@and.digital>
2022-11-25 13:18:48 +0000

    Fix root url

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,9 +1,11 @@
 import JSONAPIAdapter from '@ember-data/adapter/json-api';
+import config from 'super-rentals/config/environment';
 
 export default class ApplicationAdapter extends JSONAPIAdapter {
   namespace = 'api';
 
   buildURL(...args) {
-    return `${super.buildURL(...args)}.json`;
+    const url = super.buildURL(...args).slice(1);
+    return `${config.rootURL}${url}.json`;
   }
 }
--- config/environment.js
+++ config/environment.js
@@ -41,6 +41,7 @@ module.exports = function (environment) {
 
   if (environment === 'production') {
     // here you can enable a production-specific feature
+    ENV.rootURL = '/super-rentals/';
   }
 
   ENV.MAPBOX_ACCESS_TOKEN =
