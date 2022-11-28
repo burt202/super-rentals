1655a7c3af02976ddd42a4704a3f92f9d87ccdda
Aaron Burtnyk <aaron.burtnyk@britishgas.co.uk>
2022-11-28 13:31:26 +0000

    Update router paths

--- app/router.js
+++ app/router.js
@@ -8,6 +8,6 @@ export default class Router extends EmberRouter {
 
 Router.map(function () {
   this.route('about');
-  this.route('contact', { path: '/getting-in-touch' });
-  this.route('rental', { path: '/rentals/:rental_id' });
+  this.route('contact', { path: config.rootURL + 'getting-in-touch' });
+  this.route('rental', { path: config.rootURL + 'rentals/:rental_id' });
 });
